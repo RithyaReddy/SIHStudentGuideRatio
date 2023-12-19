01643efc66755cfabcd1cc7adb30df28e7efdff4
RithyaReddy <2020564678.pisati@ug.sharda.ac.in>
2023-12-20 02:16:02 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,73 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/ratio");
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error("Fetch error:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Guide and Student Ratios</h1>
+      {data ? (
+        <div>
+          <p>Total Students: {data.totalStudents}</p>
+          <p>Total Professors: {data.totalProfessors}</p>
+          <p>Total Associate Professors: {data.totalAssociateProfessors}</p>
+          <p>Total Assistant Professors: {data.totalAssistantProfessors}</p>
+
+          <h2>Ratios:</h2>
+          <p>
+            Total Students to Professors Ratio:{" "}
+            {data.ratioToProfessors.toFixed(2)}
+          </p>
+          <p>
+            Total Students to Associate Professors Ratio:{" "}
+            {data.ratioToAssociateProfessors.toFixed(2)}
+          </p>
+          <p>
+            Total Students to Assistant Professors Ratio:{" "}
+            {data.ratioToAssistantProfessors.toFixed(2)}
+          </p>
+
+          <h2>Guide Type Ratios:</h2>
+          <p>
+            Assistant Professor Ratio: {data.assistantProfessorRatio.toFixed(2)}
+          </p>
+          <p>
+            Associate Professor Ratio: {data.associateProfessorRatio.toFixed(2)}
+          </p>
+          <p>Professor Ratio: {data.professorRatio.toFixed(2)}</p>
+
+          <h2>Allotted Students Ratios:</h2>
+          <p>
+            Total Allotted Students to Professors:{" "}
+            {data.totalAllottedStudentsToProfessors}
+          </p>
+          <p>
+            Total Allotted Students to Associate Professors:{" "}
+            {data.totalAllottedStudentsToAssociateProfessors}
+          </p>
+          <p>
+            Total Allotted Students to Assistant Professors:{" "}
+            {data.totalAllottedStudentsToAssistantProfessors}
+          </p>
+        </div>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 }
