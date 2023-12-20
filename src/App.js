5056e2c02733d9b7b38bc95619a69cc7ac718a49
RithyaReddy <2020564678.pisati@ug.sharda.ac.in>
2023-12-20 12:07:51 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-// src/App.js
+// Import statements at the top
 import React, { useState, useEffect } from "react";
+import { PieChart, Pie, Cell, Legend } from "recharts";
+import "./App.css"; // Import the external CSS file
 
+// Component definition
 function App() {
+  // State and useEffect logic
   const [data, setData] = useState(null);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch("/api/ratio");
+        console.log("Fetching data from /api/ratio");
+        const response = await fetch("http://localhost:5000/api/ratio");
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+
         const result = await response.json();
+        console.log("Data from server:", result);
         setData(result);
       } catch (error) {
         console.error("Fetch error:", error);
@@ -18,56 +28,58 @@ function App() {
     fetchData();
   }, []);
 
+  // Define data for the pie chart
+  const pieChartData = [
+    { name: "Students", value: data?.totalStudents || 0 },
+    { name: "Guides", value: data?.totalGuides || 0 },
+  ];
+
+  // Colors for each section of the pie chart
+  const colors = ["#ff9933", "#138808"];
+
+  // Component rendering
   return (
-    <div>
-      <h1>Guide and Student Ratios</h1>
-      {data ? (
-        <div>
-          <p>Total Students: {data.totalStudents}</p>
-          <p>Total Professors: {data.totalProfessors}</p>
-          <p>Total Associate Professors: {data.totalAssociateProfessors}</p>
-          <p>Total Assistant Professors: {data.totalAssistantProfessors}</p>
+    <div className="body">
+      <div className="container">
+        <h1 className="title">Guide and Student Ratios</h1>
+        {data ? (
+          <div>
+            <h2>Ratios:</h2>
+            <p className="ratio">
+              Total Students to Guides Ratio: {data.ratio.toFixed(2)}
+            </p>
 
-          <h2>Ratios:</h2>
-          <p>
-            Total Students to Professors Ratio:{" "}
-            {data.ratioToProfessors.toFixed(2)}
-          </p>
-          <p>
-            Total Students to Associate Professors Ratio:{" "}
-            {data.ratioToAssociateProfessors.toFixed(2)}
-          </p>
-          <p>
-            Total Students to Assistant Professors Ratio:{" "}
-            {data.ratioToAssistantProfessors.toFixed(2)}
-          </p>
+            <div className="piechart">
+              <PieChart width={400} height={400}>
+                <Pie
+                  data={pieChartData}
+                  dataKey="value"
+                  cx={200}
+                  cy={200}
+                  outerRadius={80}
+                  fill="#ff9933"
+                  label
+                >
+                  {pieChartData.map((entry, index) => (
+                    <Cell
+                      key={`cell-${index}`}
+                      fill={colors[index % colors.length]}
+                    />
+                  ))}
+                </Pie>
+                <Legend />
+              </PieChart>
+            </div>
 
-          <h2>Guide Type Ratios:</h2>
-          <p>
-            Assistant Professor Ratio: {data.assistantProfessorRatio.toFixed(2)}
-          </p>
-          <p>
-            Associate Professor Ratio: {data.associateProfessorRatio.toFixed(2)}
-          </p>
-          <p>Professor Ratio: {data.professorRatio.toFixed(2)}</p>
-
-          <h2>Allotted Students Ratios:</h2>
-          <p>
-            Total Allotted Students to Professors:{" "}
-            {data.totalAllottedStudentsToProfessors}
-          </p>
-          <p>
-            Total Allotted Students to Associate Professors:{" "}
-            {data.totalAllottedStudentsToAssociateProfessors}
-          </p>
-          <p>
-            Total Allotted Students to Assistant Professors:{" "}
-            {data.totalAllottedStudentsToAssistantProfessors}
-          </p>
-        </div>
-      ) : (
-        <p>Loading...</p>
-      )}
+            {/* Pie Chart */}
+          </div>
+        ) : (
+          <p className="loading">Loading...</p>
+        )}
+      </div>
+      <div className="progress">
+        <button>In Progress</button>
+      </div>
     </div>
   );
 }
